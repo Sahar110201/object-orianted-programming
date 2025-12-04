88e40744830ecf0f22af8af4af6a3d9a76c78f3a
Sahar110201 <saharadeeb7@gmail.com>
2025-12-04 17:32:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //will be using class to create a class
 class ProductProperties { //created the base class
     constructor(name, price, quantity) { //added a method constructor and also added the three initial properties name, price and quantity
-        
+        this.name = name; //name of product
+        this.price = price; //price of product
+        this.quantity = quantity; //quantity of product
     }
 }
